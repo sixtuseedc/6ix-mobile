ad76d41f29092d91e103333f4ddc62aa28cf505a
Sixtus <6ix222222@gmail.com>
2026-07-28 14:06:21 -0300

    Add Expo push notification configuration service

---
+++ src/services/notifications.ts
@@ -0,0 +1,32 @@
+import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
+
+export async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+
+  if (finalStatus !== "granted") {
+    console.log("Failed to get push token for push notification!");
+    return;
+  }
+
+  token = (await Notifications.getExpoPushTokenAsync()).data;
+  console.log("Expo Push Token:", token);
+  return token;
+}
